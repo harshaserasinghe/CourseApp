76822c5d0f69ab1d8d72cbffa4c773b85e8daf17
Harsha Serasinghe <harshaserasinghe@outlook.com>
2020-01-24 13:25:11 +0530

    Logging implemented.

--- CourseApp.Web/Program.cs
+++ CourseApp.Web/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace CourseApp.Web
 {
@@ -29,6 +30,11 @@ namespace CourseApp.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                    .ReadFrom.Configuration(hostingContext.Configuration)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .WriteTo.Debug());
     }
 }
